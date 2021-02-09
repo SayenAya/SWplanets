8905833815296ca8d655c882a1ef1d1d316aab84
Ivelina Dimitrova <dimitrovaivi@gmail.com>
2021-02-09 18:06:35 +0200

    Commit1

--- src/app/Application.js
+++ src/app/Application.js
@@ -23,6 +23,27 @@ export default class Application extends EventEmitter {
     return EVENTS;
   }
 
+    async get events() {
+        const planets = [];
+        let response = await fetch('https://swapi.dev/api/planets/');
+
+        let data = await response.json();
+
+        planets.push(data);
+        while (data.next) {
+            response = await fetch(`${data.next}`);
+
+            data = await response.json();
+            planets.push(data);
+        }
+        const planetsCollection = planets
+            .map((x) => x.results.map((c) => c))
+            .flat();
+
+            this.data.count = planetsCollection.length;
+            this.data.planets = planetsCollection;
+    }
+
   /**
    * Initializes the app.
    * Called when the DOM has loaded. You can initiate your custom classes here
@@ -31,6 +52,8 @@ export default class Application extends EventEmitter {
    */
   async init() {
     // Initiate classes and wait for async operations here.
+
+    await this.setData();
 
     this.emit(Application.events.APP_READY);
   }
